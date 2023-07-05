42ea87b1d8a5104291ce52c7c3270815d01f128d
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-05 16:38:38 +0300

    Blank classes created

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+
+}
+
+class HomeState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
