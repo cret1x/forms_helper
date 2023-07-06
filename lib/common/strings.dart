405d779d704993ba03908cf12d96a72bc9e2a9b4
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-06 13:03:11 +0300

    Common classes implemented

---
+++ lib/common/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static const String menu = "Меню";
+  static const String import = "Импорт";
+  static const String export = "Экспорт";
+  static const String construct = "Конструктор форм";
+  static const String storage = "Хранилище";
+  static const String settings = "Настройки";
+}
---
+++ lib/common/themes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static ThemeData darkBlue = ThemeData(
+      primaryColor: Colors.orange,
+      scaffoldBackgroundColor: Color.fromRGBO(59, 63, 82, 1),
+      colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: Colors.orange,
+          onPrimary: Colors.grey[100]!,
+          secondary: Colors.orange,
+          onSecondary: Colors.orange,
+          error: Colors.redAccent,
+          onError: Colors.grey[100]!,
+          background: Colors.orange,
+          onBackground: Colors.grey[100]!,
+          surface: Colors.grey[850]!,
+          onSurface: Colors.grey[100]!,
+      ),
+  );
+}
