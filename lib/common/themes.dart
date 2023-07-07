cfa85cd1649e49790930a297a57d990863489fb2
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-07 13:44:57 +0300

    Font added

--- lib/common/themes.dart
+++ lib/common/themes.dart
@@ -2,25 +2,51 @@ import 'package:flutter/material.dart';
 
 class Themes {
   static ThemeData darkBlue = ThemeData(
-      primaryColor: Colors.orange,
-      scaffoldBackgroundColor: Color.fromRGBO(59, 63, 82, 1),
-      textTheme: const TextTheme(
-          titleMedium: TextStyle(
-              fontFamily: 'Veranda',
-          )
+    primaryColor: Colors.orange,
+    scaffoldBackgroundColor: Color.fromRGBO(59, 63, 82, 1),
+    textTheme: TextTheme(
+        headlineLarge: TextStyle(
+          fontFamily: 'Verdana',
+          color: Colors.grey[100],
+        ),
+        headlineSmall: TextStyle(
+          fontFamily: 'Verdana',
+          color: Colors.grey[100],
+        ),
+        titleMedium: TextStyle(
+          fontFamily: 'Verdana',
+          color: Colors.grey[100],
+        ),
+        labelLarge: const TextStyle(
+          fontFamily: 'Verdana',
+          fontSize: 14,
+          letterSpacing: 0.6,
+        )),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        fixedSize: MaterialStateProperty.all(
+          Size.fromHeight(50),
+        ),
+        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 28),),
       ),
-      colorScheme: ColorScheme(
-          brightness: Brightness.dark,
-          primary: Colors.orange,
-          onPrimary: Colors.grey[100]!,
-          secondary: Colors.orange,
-          onSecondary: Colors.orange,
-          error: Colors.redAccent,
-          onError: Colors.grey[100]!,
-          background: Colors.orange,
-          onBackground: Colors.grey[100]!,
-          surface: Colors.grey[850]!,
-          onSurface: Colors.grey[100]!,
-      ),
+    ),
+    colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: Color.fromRGBO(73, 73, 73, 1),
+      onPrimary: Colors.white,
+      secondary: Colors.orange,
+      onSecondary: Colors.orange,
+      error: Colors.redAccent,
+      onError: Colors.grey[100]!,
+      background: Colors.orange,
+      onBackground: Colors.grey[100]!,
+      surface: Colors.grey[850]!,
+      onSurface: Colors.grey[100]!,
+    ),
   );
 }
---
+++ lib/screens/construct.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:forms_helper/common/strings.dart';
+
+import '../common/themes.dart';
+
+class FormConstructor extends StatefulWidget {
+  const FormConstructor({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FormConstructorState();
+  }
+}
+
+class _FormConstructorState extends State<FormConstructor> {
+  final TextEditingController _editingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: Themes.darkBlue,
+      home: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 26.0, horizontal: 28),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              Strings.construct,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            IntrinsicHeight(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _editingController,
+                          cursorColor: Theme.of(context).colorScheme.onPrimary,
+                          decoration: InputDecoration(
+                            hintText: Strings.header,
+                            hintStyle:
+                                Theme.of(context).textTheme.titleMedium!.copyWith(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium!
+                                          .color!
+                                          .withOpacity(0.4),
+                                    ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            filled: true,
+                            contentPadding: const EdgeInsets.all(16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                    child: VerticalDivider(
+                     thickness: 1,
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              Strings.questions,
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                Strings.add,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
