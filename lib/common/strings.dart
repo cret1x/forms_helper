4d3deceb74aa466c08f2b1fb80b3056bcec66dd3
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-07 18:21:26 +0300

    Common classes updated

--- lib/common/strings.dart
+++ lib/common/strings.dart
@@ -6,6 +6,16 @@ class Strings {
   static const String storage = "Хранилище";
   static const String settings = "Настройки";
   static const String header = "Заголовок";
+  static const String description = "Описание";
   static const String questions = "Вопросы";
   static const String add = "Добавить";
+  static const String clear = "Очистить";
+  static const String clearForm = "Очистить форму";
+  static const String save = "Сохранить";
+  static const String clearConfirmation =
+      "Вы уверены, что хотите отменить все внесённые изменения?\n"
+      "Это действие нельзя будет отменить.";
+  static const String yes = "Да";
+  static const String no = "Нет";
+  static const String noQuestions = "Пока не добавлено ни одного вопроса";
 }
--- lib/common/themes.dart
+++ lib/common/themes.dart
@@ -2,26 +2,36 @@ import 'package:flutter/material.dart';
 
 class Themes {
   static ThemeData darkBlue = ThemeData(
-    primaryColor: Colors.orange,
     scaffoldBackgroundColor: Color.fromRGBO(59, 63, 82, 1),
+    textSelectionTheme: TextSelectionThemeData(
+      selectionHandleColor: Colors.white,
+      selectionColor: Colors.grey[850]!,
+    ),
     textTheme: TextTheme(
-        headlineLarge: TextStyle(
-          fontFamily: 'Verdana',
-          color: Colors.grey[100],
-        ),
-        headlineSmall: TextStyle(
-          fontFamily: 'Verdana',
-          color: Colors.grey[100],
-        ),
-        titleMedium: TextStyle(
-          fontFamily: 'Verdana',
-          color: Colors.grey[100],
-        ),
-        labelLarge: const TextStyle(
-          fontFamily: 'Verdana',
-          fontSize: 14,
-          letterSpacing: 0.6,
-        )),
+      headlineLarge: TextStyle(
+        fontFamily: 'Verdana',
+        color: Colors.grey[100],
+      ),
+      headlineSmall: TextStyle(
+        fontFamily: 'Verdana',
+        color: Colors.grey[100],
+      ),
+      titleMedium: TextStyle(
+        fontFamily: 'Verdana',
+        color: Colors.grey[100],
+      ),
+      labelLarge: const TextStyle(
+        fontFamily: 'Verdana',
+        fontSize: 14,
+        letterSpacing: 0.6,
+      ),
+      displaySmall: TextStyle(
+        fontFamily: 'Verdana',
+        fontSize: 18,
+        fontWeight: FontWeight.w100,
+        color: Colors.grey[100]!.withOpacity(0.6),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         shape: MaterialStateProperty.all(
@@ -30,9 +40,25 @@ class Themes {
           ),
         ),
         fixedSize: MaterialStateProperty.all(
-          Size.fromHeight(50),
+          const Size.fromHeight(50),
         ),
-        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 28),),
+        padding: const MaterialStatePropertyAll(
+          EdgeInsets.symmetric(horizontal: 28),
+        ),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(
+          Colors.white,
+        ),
+        textStyle: const MaterialStatePropertyAll(
+          TextStyle(
+            fontFamily: 'Verdana',
+            fontSize: 16,
+            letterSpacing: 0.6,
+          ),
+        ),
       ),
     ),
     colorScheme: ColorScheme(
@@ -46,7 +72,7 @@ class Themes {
       background: Colors.orange,
       onBackground: Colors.grey[100]!,
       surface: Colors.grey[850]!,
-      onSurface: Colors.grey[100]!,
+      onSurface: Colors.grey[400]!,
     ),
   );
 }
