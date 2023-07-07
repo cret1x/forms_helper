a229c2fd0d1bb0264917d9c29a3d98a344b0d5de
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-07 18:21:12 +0300

    Constructor screen implemented

--- lib/screens/construct.dart
+++ lib/screens/construct.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:forms_helper/common/strings.dart';
 
 import '../common/themes.dart';
+import '../entities/question.dart';
 
 class FormConstructor extends StatefulWidget {
   const FormConstructor({super.key});
@@ -12,8 +13,11 @@ class FormConstructor extends StatefulWidget {
   }
 }
 
-class _FormConstructorState extends State<FormConstructor> with AutomaticKeepAliveClientMixin<FormConstructor> {
-  final TextEditingController _editingController = TextEditingController();
+class _FormConstructorState extends State<FormConstructor>
+    with AutomaticKeepAliveClientMixin<FormConstructor> {
+  final TextEditingController _headerController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final List<Question> _questions = [];
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +33,7 @@ class _FormConstructorState extends State<FormConstructor> with AutomaticKeepAli
               Strings.construct,
               style: Theme.of(context).textTheme.headlineLarge,
             ),
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
             IntrinsicHeight(
@@ -39,18 +43,20 @@ class _FormConstructorState extends State<FormConstructor> with AutomaticKeepAli
                     child: Column(
                       children: [
                         TextField(
-                          controller: _editingController,
+                          controller: _headerController,
                           cursorColor: Theme.of(context).colorScheme.onPrimary,
                           decoration: InputDecoration(
                             hintText: Strings.header,
-                            hintStyle:
-                                Theme.of(context).textTheme.titleMedium!.copyWith(
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium!
-                                          .color!
-                                          .withOpacity(0.4),
-                                    ),
+                            hintStyle: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium!
+                                      .color!
+                                      .withOpacity(0.4),
+                                ),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(15),
                               borderSide: const BorderSide(
@@ -62,13 +68,119 @@ class _FormConstructorState extends State<FormConstructor> with AutomaticKeepAli
                             contentPadding: const EdgeInsets.all(16),
                           ),
                         ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        TextFormField(
+                          controller: _descriptionController,
+                          cursorColor: Theme.of(context).colorScheme.onPrimary,
+                          maxLines: 12,
+                          decoration: InputDecoration(
+                            hintText: Strings.description,
+                            hintStyle: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium!
+                                      .color!
+                                      .withOpacity(0.4),
+                                ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            filled: true,
+                            contentPadding: const EdgeInsets.all(16),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                style: const ButtonStyle(
+                                  backgroundColor: MaterialStatePropertyAll(
+                                      Colors.redAccent),
+                                ),
+                                onPressed: () async {
+                                  bool? result = await showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      actionsPadding: EdgeInsets.all(12),
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(15),
+                                        ),
+                                      ),
+                                      title: const Text(
+                                        Strings.clearForm,
+                                        style: TextStyle(
+                                          fontFamily: 'Verdana',
+                                        ),
+                                      ),
+                                      content: Text(
+                                        Strings.clearConfirmation,
+                                        style: TextStyle(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .onSurface),
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context, true);
+                                          },
+                                          child: Text(Strings.yes),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context, false);
+                                          },
+                                          child: Text(Strings.no),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                  if (result != null && result == true) {
+                                    setState(() {
+                                      _headerController.text = "";
+                                      _descriptionController.text = "";
+                                      _questions.clear();
+                                    });
+                                  }
+                                },
+                                child: const Text(
+                                  Strings.clear,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 24,
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  Strings.save,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
                       ],
                     ),
                   ),
                   const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
                     child: VerticalDivider(
-                     thickness: 1,
+                      thickness: 1,
                     ),
                   ),
                   Expanded(
@@ -84,12 +196,28 @@ class _FormConstructorState extends State<FormConstructor> with AutomaticKeepAli
                             ),
                             ElevatedButton(
                               onPressed: () {},
-                              child: Text(
+                              child: const Text(
                                 Strings.add,
                               ),
                             )
                           ],
                         ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        _questions.isEmpty
+                            ? Center(
+                                child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 72),
+                                child: Text(
+                                  Strings.noQuestions,
+                                  textAlign: TextAlign.center,
+                                  style:
+                                      Theme.of(context).textTheme.displaySmall,
+                                ),
+                              ))
+                            : Container(),
                       ],
                     ),
                   ),
