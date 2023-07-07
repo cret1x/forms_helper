1112b625fde3ce0852e459e73016f66828e0a177
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-07 18:20:51 +0300

    Made Form constructor widget const

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -30,7 +30,7 @@ class _HomeState extends State<HomeWidget> {
                 children: [
                   Container(color: Colors.red,),
                   Container(color: Colors.blue,),
-                  FormConstructor(),
+                  const FormConstructor(),
                   Container(color: Colors.green,),
                   Container(color: Colors.orange,),
                 ],
