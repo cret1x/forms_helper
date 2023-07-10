148459fca862d7946d6a21fa3a053d20f29efeaa
cret1x <33805149+cret1x@users.noreply.github.com>
2023-07-10 16:10:53 +0300

    Fix scroll

--- lib/screens/form_view.dart
+++ lib/screens/form_view.dart
@@ -17,89 +17,91 @@ class FormView extends StatefulWidget {
 class _FormViewState extends State<FormView> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            SizedBox(
-              width: 500,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        Strings.name,
-                        style: Theme.of(context).textTheme.titleMedium,
+    return Expanded(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 500,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      Strings.name,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(
+                      width: 36,
+                    ),
+                    Expanded(
+                      child: SelectableText(
+                        widget._form.documentTitle,
+                        style: Theme.of(context).textTheme.displaySmall,
+                        minLines: 1,
+                        maxLines: 18,
+                        textAlign: TextAlign.end,
                       ),
-                      const SizedBox(
-                        width: 36,
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      Strings.header,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(
+                      width: 36,
+                    ),
+                    Expanded(
+                      child: SelectableText(
+                        widget._form.title,
+                        style: Theme.of(context).textTheme.displaySmall,
+                        minLines: 1,
+                        maxLines: 18,
+                        textAlign: TextAlign.end,
                       ),
-                      Expanded(
-                        child: SelectableText(
-                          widget._form.documentTitle,
-                          style: Theme.of(context).textTheme.displaySmall,
-                          minLines: 1,
-                          maxLines: 18,
-                          textAlign: TextAlign.end,
-                        ),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      Strings.description,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(
+                      width: 36,
+                    ),
+                    Expanded(
+                      child: SelectableText(
+                        widget._form.description,
+                        style: Theme.of(context).textTheme.displaySmall,
+                        minLines: 1,
+                        maxLines: 18,
+                        textAlign: TextAlign.end,
                       ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        Strings.header,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      const SizedBox(
-                        width: 36,
-                      ),
-                      Expanded(
-                        child: SelectableText(
-                          widget._form.title,
-                          style: Theme.of(context).textTheme.displaySmall,
-                          minLines: 1,
-                          maxLines: 18,
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        Strings.description,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      const SizedBox(
-                        width: 36,
-                      ),
-                      Expanded(
-                        child: SelectableText(
-                          widget._form.description,
-                          style: Theme.of(context).textTheme.displaySmall,
-                          minLines: 1,
-                          maxLines: 18,
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Expanded(
-              child: Column(),
+          ),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: widget._form.questions?.map((e) => Column(children: [Text(e.title), Text(e.description)],)).toList() ?? [],
             ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
