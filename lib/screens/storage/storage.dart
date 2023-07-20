81760479d670c2748c6d4c5b80de575968947f87
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-20 17:41:01 +0300

    Refactor

--- lib/screens/storage/storage.dart
+++ lib/screens/storage/storage.dart
@@ -7,11 +7,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forms_helper/screens/construct.dart';
 import 'package:forms_helper/screens/common_widgets/question_item_widget.dart';
 
-import '../common/strings.dart';
-import '../common/themes.dart';
-import '../entities/question_item.dart';
-import '../global_providers.dart';
-import '../sqlite/local_storage.dart';
+import '../../common/strings.dart';
+import '../../common/themes.dart';
+import '../../entities/question_item.dart';
+import '../../entities/question_tag.dart';
+import '../../global_providers.dart';
+import '../../sqlite/local_storage.dart';
 
 class StorageWidget extends ConsumerStatefulWidget {
   final PageController pageController;
@@ -35,12 +36,14 @@ class _StorageWidgetState extends ConsumerState<StorageWidget>
         SingleTickerProviderStateMixin {
   final TextEditingController _controller = TextEditingController();
   final LocalStorage _storage = LocalStorage();
-  String? _dropdownValue;
+  Tag? _dropdownValue;
   late Animation<double> _animation;
   late AnimationController _animationController;
   List<QuestionItemWidget>? _qWidgets;
   late List<QuestionItem> _constructQuestions;
   int _page = 1;
+  final Tag _nullTag = Tag(id: "", value: Strings.notSelected);
+  List<Tag>? _disciplines;
 
   @override
   void initState() {
@@ -70,8 +73,12 @@ class _StorageWidgetState extends ConsumerState<StorageWidget>
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    _disciplines = ref.watch(disciplinesProvider);
+    _dropdownValue ??= _nullTag;
     Future<List<QuestionItem>> _questions = _storage.getQuestions(
-        searchText: _controller.text, page: _page - 1, tag: _dropdownValue);
+        searchText: _controller.text,
+        page: _page - 1,
+        tag: _dropdownValue == _nullTag ? null : _dropdownValue);
     return MaterialApp(
       theme: Themes.darkBlue,
       home: Padding(
@@ -86,82 +93,42 @@ class _StorageWidgetState extends ConsumerState<StorageWidget>
             const SizedBox(
               height: 24,
             ),
-            TextField(
-              controller: _controller,
-              cursorColor: Theme.of(context).colorScheme.onPrimary,
-              onChanged: (_) {
-                setState(() {});
-              },
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-                hintText: Strings.findQuestion,
-                hintStyle: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      color: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .color!
-                          .withOpacity(0.4),
-                    ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                filled: true,
-                contentPadding: const EdgeInsets.all(16),
-                prefixIconConstraints: const BoxConstraints(
-                  minWidth: 50,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
             Row(
               children: [
-                Flexible(
-                  child: DropdownButtonFormField(
-                    decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(16),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    cursorColor: Theme.of(context).colorScheme.onPrimary,
+                    onChanged: (_) {
+                      setState(() {});
+                    },
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                      hintText: Strings.findQuestion,
+                      hintStyle:
+                          Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .color!
+                                    .withOpacity(0.4),
+                              ),
                       border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
                         ),
                       ),
                       filled: true,
+                      contentPadding: const EdgeInsets.all(16),
+                      prefixIconConstraints: const BoxConstraints(
+                        minWidth: 50,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(15),
-                    style: Theme.of(context).textTheme.titleMedium,
-                    hint: Text(
-                      Strings.discipline,
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                            color: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .color!
-                                .withOpacity(0.4),
-                          ),
-                    ),
-                    value: _dropdownValue,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _dropdownValue = newValue!;
-                      });
-                    },
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: <String>['Apple', 'Mango', 'Banana', 'Peach']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
                   ),
                 ),
                 const SizedBox(
@@ -226,10 +193,62 @@ class _StorageWidgetState extends ConsumerState<StorageWidget>
                         },
                   child: const Icon(Icons.keyboard_double_arrow_right_rounded),
                 ),
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: DropdownButtonFormField<Tag>(
+                    borderRadius: BorderRadius.circular(15),
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(16),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      filled: true,
+                    ),
+                    value: _dropdownValue,
+                    onChanged: (Tag? newValue) {
+                      setState(() {
+                        _dropdownValue = newValue!;
+                      });
+                    },
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: [_nullTag, ..._disciplines!]
+                        .map<DropdownMenuItem<Tag>>((Tag value) {
+                      return DropdownMenuItem<Tag>(
+                        value: value,
+                        child: Text(value.value),
+                      );
+                    }).toList(),
+                  ),
+                ),
                 const SizedBox(
                   width: 12,
                 ),
                 ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(
+                    Icons.edit_outlined,
+                  ),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(
+                      EdgeInsets.symmetric(
+                        horizontal: 24,
+                      ),
+                    ),
+                  ),
                   onPressed: () {
                     _constructQuestions = ref.read(constructorProvider);
                     final selectedQuestions = ref.read(storageProvider);
