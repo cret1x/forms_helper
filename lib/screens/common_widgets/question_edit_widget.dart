4064f88bec43c102dbe64616f2a9e9813a14ef5a
vngldkh <90322828+vngldkh@users.noreply.github.com>
2023-07-21 21:19:54 +0300

    Blank widget for editing and creating question added

---
+++ lib/screens/common_widgets/question_edit_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class QuestionEditWidget extends ConsumerStatefulWidget {
+  const QuestionEditWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _QuestionEditWidgetState();
+  }
+}
+
+class _QuestionEditWidgetState extends ConsumerState<QuestionEditWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
